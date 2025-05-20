86a61a394ea00e95638b22e2040cfbda1d4fc7b0
Alejo <daniel.acero@mail.escuelaing.edu.co>
2025-05-20 14:47:36 -0500

    Test 4 Back ground image correcto

--- my-app/src/App.test.jsx
+++ my-app/src/App.test.jsx
@@ -1,26 +1,36 @@
+// src/App.test.jsx
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
 describe('App (smoke test)', () => {
-  it('monta el componente sin fallar y muestra el botón Empezar', async () => {
+  it('Test 1 – monta el componente sin fallar y muestra el botón Empezar', async () => {
     const { container } = render(<App />);
     const startBtn = screen.getByRole('button', { name: /empezar/i });
     expect(startBtn).toBeInTheDocument();
+    console.log('✅ Test 1 App Mount');
   });
 
-  it('renderiza el elemento audio con loop y la ruta correcta', () => {
+  it('Test 2 – renderiza el elemento audio con loop y la ruta correcta', () => {
     const { container } = render(<App />);
     const audio = container.querySelector('audio');
-    // debe existir
     expect(audio).toBeInTheDocument();
-    // tiene loop
     expect(audio).toHaveAttribute('loop');
-    // src coincide con tu fichero de música
     expect(audio).toHaveAttribute(
       'src',
       '/Bomberman (NES) Music - Stage Theme.mp3'
     );
+    console.log('✅ Test 2 Audio App');
+  });
+
+  it('Test 3 – el contenedor .homepage-container tiene el background-image correcto', () => {
+    const { container } = render(<App />);
+    const homepage = container.querySelector('.homepage-container');
+    expect(homepage).toBeInTheDocument();
+    expect(homepage).toHaveStyle(
+      'background-image: url("/src/images/f1.png")'
+    );
+    console.log('✅ Test 3 Background Image');
   });
 });
